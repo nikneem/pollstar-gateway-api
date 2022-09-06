dc20dc3c42a82906bf7577d9909ae44730bc9e63
Eduard <e.keilholz@4dotnet.nl>
2022-09-06 10:09:00 +0200

    Enable CORS

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-//app.use(cors(corsOptionsDelegate));
+app.use(cors(corsOptionsDelegate));
 
 // expose the swagger.json OpenAPI definition
 app.use(express.static(path.join(__dirname, "open-api")));
